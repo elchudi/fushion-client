6f5edeec95e5bda7ab302246881b8b2f952e6ff3
Xavi Fuentes Fernández <xavifuefer@xavis-macbookpro.home>
2014-03-23 22:30:12 +0000

    structure created from angular boilerplate

---
+++ app.js
@@ -0,0 +1,21 @@
+var app = angular.module('app', [
+    'ui.bootstrap',
+    'ui.utils',
+    'ui.router',
+    'ngAnimate'
+]);
+
+angular.module('app').run(function($rootScope) {
+
+    $rootScope.safeApply = function(fn) {
+        var phase = $rootScope.$$phase;
+        if (phase === '$apply' || phase === '$digest') {
+            if (fn && (typeof(fn) === 'function')) {
+                fn();
+            }
+        } else {
+            this.$apply(fn);
+        }
+    };
+
+});
---
+++ scripts/routes.js
@@ -0,0 +1,14 @@
+angular.module('app').config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise("/");
+
+    $stateProvider
+        .state('home', {
+            url: "/",
+            views: {
+                "navbar": { templateUrl: "partial/navbar/navbar.html" },
+                "content": { templateUrl: "modules/home/home.html" }
+            }
+        })
+    ;
+});
